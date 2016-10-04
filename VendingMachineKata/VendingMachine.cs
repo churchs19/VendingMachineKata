8f3732c51b98e6d5c5f53f2affc22eebf142fe5f
Shane Church <shane@s-church.net>
2016-10-04 18:46:30 -0500

    Completed insert coin method and tests

--- VendingMachineKata/VendingMachine.cs
+++ VendingMachineKata/VendingMachine.cs
@@ -10,14 +10,33 @@ namespace VendingMachineKata
     {
         public VendingMachine()
         {
-            Display = "INSERT COIN";
         }
 
-        public string Display { get; private set; }
+        private double coinValue = 0;
+        public string Display
+        {
+            get
+            {
+                return coinValue == 0 ? "INSERT COIN" : coinValue.ToString("C2");
+            }
+        }
 
         public bool InsertCoin(string coin)
         {
-            return false;
+            switch(coin)
+            {
+                case "q":
+                    coinValue += 0.25;
+                    return true;
+                case "d":
+                    coinValue += 0.10;
+                    return true;
+                case "n":
+                    coinValue += 0.05;
+                    return true;
+                default:
+                    return false;
+            }
         }
     }
 }
--- VendingMachineTests/AcceptCoinTests.cs
+++ VendingMachineTests/AcceptCoinTests.cs
@@ -28,5 +28,44 @@ namespace VendingMachineTests
             Assert.IsTrue(accepted);
             Assert.AreEqual(machine.Display, "$0.25");
         }
+
+        [TestMethod]
+        public void InsertDime()
+        {
+            bool accepted = machine.InsertCoin("d");
+            Assert.IsTrue(accepted);
+            Assert.AreEqual(machine.Display, "$0.10");
+        }
+
+        [TestMethod]
+        public void InsertNickel()
+        {
+            bool accepted = machine.InsertCoin("n");
+            Assert.IsTrue(accepted);
+            Assert.AreEqual(machine.Display, "$0.05");
+        }
+
+        [TestMethod]
+        public void InsertPenny()
+        {
+            bool accepted = machine.InsertCoin("p");
+            Assert.IsFalse(accepted);
+            Assert.AreEqual("INSERT COIN", machine.Display);
+        }
+
+        [TestMethod]
+        public void InsertQuarterDimeNickelAndPenny()
+        {
+            bool accepted = machine.InsertCoin("q");
+            Assert.IsTrue(accepted);
+            accepted = machine.InsertCoin("d");
+            Assert.IsTrue(accepted);
+            accepted = machine.InsertCoin("n");
+            Assert.IsTrue(accepted);
+            accepted = machine.InsertCoin("p");
+            Assert.IsFalse(accepted);
+            Assert.AreEqual("$0.40", machine.Display);
+        }
+
     }
 }
