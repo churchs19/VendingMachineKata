b0130c43ae2841a343be66be22ae6e948bbfe1f3
Shane Church <shane@s-church.net>
2016-10-04 18:16:09 -0500

    Initial failing display test

---
+++ VendingMachineTests/AcceptCoinTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTests
+{
+    [TestClass]
+    public class AcceptCoinTests
+    {
+        [TestMethod]
+        public void InitialDisplayReadsINSERTCOIN()
+        {
+            Assert.Fail();
+        }
+    }
+}
